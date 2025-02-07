21646e6d29fbe96f151556b8382fa4c6307b1760
Aleksey Pleshkov <alekseypleshkov@icloud.com>
2025-02-07 21:48:34 +0200

    Initial Commit

---
+++ Localizator/LocalizatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizatorApp.swift
+//  Localizator
+//
+//  Created by Aleksey Pleshkov on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalizatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
